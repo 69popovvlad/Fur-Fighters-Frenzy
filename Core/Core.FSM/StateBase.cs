75ab7591c385b6c8655aa5e91a6f9a6e01c401a5
Validosik <69popovvlad@gmail.com>
2024-03-10 16:16:20 +0300

    Core: add FSM

---
+++ Core/Core.FSM/StateBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Core.FSM
+{
+    public abstract class StateBase<T> where T: Enum
+    {
+        public T StateKey { get; private set; }
+
+        protected StateBase(T stateKey)
+        {
+            StateKey = stateKey;
+        }
+
+        public abstract void EnterState();
+
+        public abstract void UpdateState(float delta);
+
+        public abstract void ExitState();
+
+        public abstract T GetNextStateKey();
+    }
+}
---
+++ Core/Core.FSM/StateMachine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.FSM
+{
+    public abstract class StateMachine<T> where T: Enum
+    {
+        protected readonly Dictionary<T, StateBase<T>> States = new Dictionary<T, StateBase<T>>();
+
+        protected StateBase<T> CurrentState;
+        protected bool IsTransitioning;
+
+        public virtual void Update(float delta)
+        {
+            var nextStateKey = CurrentState.GetNextStateKey();
+            if (nextStateKey.Equals(CurrentState.StateKey))
+            {
+                CurrentState.UpdateState(delta);    
+                return;
+            }
+            
+            
+            if(!IsTransitioning)
+            {
+                TransitionToState(nextStateKey);
+            }
+        }
+
+        protected void Start()
+        {
+            CurrentState.EnterState();
+        }
+
+        protected void AddState(StateBase<T> state)
+        {
+            States[state.StateKey] = state;
+        }
+
+        private void TransitionToState(T stateKey)
+        {
+            IsTransitioning = true;
+            {
+                CurrentState.ExitState();
+                CurrentState = States[stateKey];
+                CurrentState.EnterState();
+            }
+            IsTransitioning = false;
+        }
+    }
+}
