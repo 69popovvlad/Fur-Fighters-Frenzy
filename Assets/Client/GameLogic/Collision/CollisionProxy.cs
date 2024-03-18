6edcd23a58ce18cd12b8594c20ae44fe8718cdcc
Validosik <69popovvlad@gmail.com>
2024-03-18 14:06:53 +0300

    Released punching with hitboxes

--- Assets/Client/GameLogic/Collision/CollisionProxy.cs
+++ Assets/Client/GameLogic/Collision/CollisionProxy.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,18 +6,25 @@ namespace Client.GameLogic.Collision
 {
     public class CollisionProxy : ColliderDataControl
     {
-        [SerializeField]
-        private UnityEvent<string, string, ColliderDataControl>[] _onCollisionEnter =
+        [SerializeField] private Collider _collider;
+
+        [SerializeField] private UnityEvent<string, string, ColliderDataControl>[] _onCollisionEnter =
             Array.Empty<UnityEvent<string, string, ColliderDataControl>>();
 
         private bool _enabled;
-        
+
+        private void Awake()
+        {
+            Enable(_enabled);
+        }
+
         public void Enable(bool enable)
         {
             _enabled = enable;
+            _collider.enabled = enable;
         }
-        
-        private void OnCollisionEnter(UnityEngine.Collision other)
+
+        private void OnTriggerEnter(Collider other)
         {
             if (!_enabled
                 || !other.transform.TryGetComponent<ColliderDataControl>(out var colliderData)
@@ -27,36 +33,12 @@ namespace Client.GameLogic.Collision
                 return;
             }
 
+            Enable(false);
+
             for (int i = 0, iLen = _onCollisionEnter.Length; i < iLen; ++i)
             {
                 _onCollisionEnter[i].Invoke(CharacterEntityKey, OnCollisionEnterKey, colliderData);
             }
-            
-#if UNITY_EDITOR
-            _collisions.Add((other.contacts[0].point, 5));
-#endif
-            
         }
-
-#if UNITY_EDITOR
-
-        private readonly List<(Vector3 position, float liveDuration)> _collisions = new List<(Vector3, float)>();
-        
-        private void OnDrawGizmos()
-        {
-            for (int i = 0, iLen = _collisions.Count; i < iLen; ++i)
-            {
-                var point = _collisions[i];
-                if (point.liveDuration <= 0)
-                {
-                    continue;   
-                }
-                
-                Gizmos.DrawSphere(point.position, 0.05f);
-
-                point.liveDuration -= Time.deltaTime;
-            }
-        }
-#endif
     }
 }
