ba2ce23934c62ee1d7c6c087fdbd1b238431e046
Validosik <69popovvlad@gmail.com>
2024-03-18 19:03:06 +0300

    Released spawn and transform sync for the Network

---
+++ Assets/Client/Network/GameLogic/CharacterSpawner.cs
@@ -0,0 +1,23 @@
+﻿using Client.GameLogic.Inputs;
+using Core.Assets;
+using Core.Ioc;
+using FishNet.Object;
+using UnityEngine;
+
+namespace Client.Network.GameLogic
+{
+    public class CharacterSpawner : NetworkBehaviour
+    {
+        [SerializeField] private PlayerInputHandler _inputPrefab;
+
+        public override void OnStartClient()
+        {
+            var input = Instantiate(_inputPrefab);
+
+            var assetsLoader = Ioc.Instance.Get<AssetsLoader>();
+            var prefab = assetsLoader.LoadResource<GameObject>("character", "cat_000"); // TODO: Load key or character data
+            var instance = Instantiate(prefab);
+            ServerManager.Spawn(instance, Owner);
+        }
+    }
+}
---
+++ Assets/Client/Network/GameLogic/Characters/CheckOwnerControl.cs
@@ -0,0 +1,26 @@
+﻿using FishNet.Object;
+using UnityEngine;
+
+namespace Client.Network.GameLogic.Characters
+{
+    public class CheckOwnerControl : NetworkBehaviour
+    {
+        [SerializeField, Tooltip("Components that should only be on the client character")]
+        private Component[] _onlyOwnerComponents;
+
+        public override void OnStartClient()
+        {
+            if (IsOwner)
+            {
+                return;
+            }
+
+            for (int i = 0, iLen = _onlyOwnerComponents.Length; i < iLen; ++i)
+            {
+                Destroy(_onlyOwnerComponents[i]);
+            }
+
+            _onlyOwnerComponents = null;
+        }
+    }
+}
