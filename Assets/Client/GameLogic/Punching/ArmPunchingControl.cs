b80f454eebfe94238c52fce4dd9075d43e690519
Validosik <69popovvlad@gmail.com>
2024-03-23 11:48:42 +0300

    Updated punching control to use a delay and a smoothing curve

--- Assets/Client/GameLogic/Punching/ArmPunchingControl.cs
+++ Assets/Client/GameLogic/Punching/ArmPunchingControl.cs
@@ -10,28 +10,52 @@ namespace Client.GameLogic.Punching
     {
         [SerializeField] private ChainIKConstraint _armIK;
         [SerializeField] private CollisionProxy _armCollision;
-        [SerializeField] private float _comebackDuration = 1;
-        
+        [SerializeField] private AnimationCurve _punchCurve = AnimationCurve.Linear(0, 0, 1, 1);
+        [SerializeField] private float _punchDuration = 0.2f;
+        [SerializeField] private float _comebackDuration = 0.3f;
+
+        private bool _inPunching;
+        private float _punchT;
+
         private void Update()
         {
-            if (_armIK.weight <= 0)
+            if (!_inPunching)
+            {
+                CalculatePunchReturn();
+                return;
+            }
+
+            CalculatePunch();
+        }
+
+        private void CalculatePunch()
+        {
+            if (_punchT >= 1)
+            {
+                _inPunching = false;
+                return;
+            }
+
+            _punchT += Time.deltaTime / _punchDuration;
+            _armIK.weight = _punchCurve.Evaluate(_punchT);
+        }
+
+        private void CalculatePunchReturn()
+        {
+            if (_punchT <= 0)
             {
                 return;
             }
 
-            _armIK.weight -= Time.deltaTime / _comebackDuration;
-            
-            if (_armIK.weight > 0)
+            _punchT -= Time.deltaTime / _comebackDuration;
+            _armIK.weight = _punchCurve.Evaluate(_punchT);
+
+            if (_punchT > 0)
             {
                 return;
             }
-            
-            if (!IsOwner)
-            {
-                return;
-            }
-            
-            SetWeightToServer(0);
+
+            _armCollision.Enable(false);
         }
 
         internal void Punch(in PunchInputCommand command)
@@ -41,20 +65,26 @@ namespace Client.GameLogic.Punching
                 return;
             }
 
-            SetWeightToServer(1);
+            SetWeightToServer();
         }
 
-        [ServerRpc]
-        private void SetWeightToServer(float weight)
+        [ServerRpc(RequireOwnership = true)]
+        private void SetWeightToServer()
         {
-            SetWeightOnClients(weight);
+            SetWeightOnClients();
         }
 
         [ObserversRpc(RunLocally = true)]
-        private void SetWeightOnClients(float weight)
+        private void SetWeightOnClients()
         {
-            _armIK.weight = weight;
-            _armCollision.Enable(weight > 0);
+            if (_punchT > 0)
+            {
+                return;
+            }
+            
+            _inPunching = true;
+            _armCollision.Enable(true);
+            _punchT = 0;
         }
     }
 }
